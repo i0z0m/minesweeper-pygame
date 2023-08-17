7340b166e2ab8b6b153453bc85b86908514af72d
i0z0m <i0z0mu@gmail.com>
2023-08-18 02:12:12 +0900

    refactoring

--- src/minesweeper_pygame/__main__.py
+++ src/minesweeper_pygame/__main__.py
@@ -1,54 +1,40 @@
 import pygame
 
 # ゲームの設定
-WIDTH = 400
-HEIGHT = 400
-ROWS = 20
-COLS = 20
+WIDTH, HEIGHT = 400, 400
+ROWS, COLS = 20, 20
 CELL_SIZE = WIDTH // COLS
 
 # 色の定義
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
-RED = (255, 0, 0)
 
-# 初期化
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Minesweeper")
-
-# ゲームループ
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # 左クリック
-                x, y = event.pos
-                row = y // CELL_SIZE
-                col = x // CELL_SIZE
-                print(f"Clicked cell: ({row}, {col})")
-
-    screen.fill(WHITE)
-
-    # グリッドを描画
+def draw_grid(screen, cell_size):
+    """グリッドを描画する関数"""
     for row in range(ROWS):
         for col in range(COLS):
-            rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-            pygame.draw.rect(screen, GRAY, rect, 1)
+            pygame.draw.rect(screen, GRAY, (col * cell_size, row * cell_size, cell_size, cell_size), 1)
 
-    # クリックされたセルを赤色で塗りつぶす
-    mouse_pos = pygame.mouse.get_pos()
-    mouse_row = mouse_pos[1] // CELL_SIZE
-    mouse_col = mouse_pos[0] // CELL_SIZE
-    rect = pygame.Rect(
-        mouse_col * CELL_SIZE, mouse_row * CELL_SIZE, CELL_SIZE, CELL_SIZE
-    )
-    pygame.draw.rect(screen, RED, rect)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Minesweeper")
+    clock = pygame.time.Clock()
 
-    pygame.display.flip()
+    # ゲームループ
+    running = True
+    while running:
+        screen.fill(WHITE)
+        draw_grid(screen, CELL_SIZE)
+        pygame.display.flip()
 
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        clock.tick(60)
 
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
